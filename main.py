65eab7cdf837a8b605d710e753152ba9c05b1950
Vishwa <vishwagreat24@gmail.com>
2026-06-11 13:27:39 +0530

    done

--- main.py
+++ main.py
@@ -55,7 +55,38 @@ Question:{question}
 
 Answer:
 """
+prompt = prompt_template(
+    template=template,
+    input_variable=["context","question"]
+)
 
+# Build RAG Chain 
 
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=retriever,
+    chain_type_kwargs={
+        "prompt": prompt
+    },
+    return_source_documents=True
+)
+
+# Chat Loop
+
+print("\n=== Enterprise RAG Assistant ===")
+print("Type 'exit' to Quit.\n")
+
+while True:
+    query = input("What Would YOU like Know: ")
+
+    if query.lower() == "exit":
+        break
+
+    result = qa_chain.invoke({"query": query})
+
+    print("\nAnswer:")
+    print(result["result"])
+    
 
 
