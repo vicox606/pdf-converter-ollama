2e1e2654a4980c9a59c72b69b3f517ea9c811d61
Vishwa <vishwagreat24@gmail.com>
2026-06-11 12:44:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from langchain_community.document_loaders import PyPDFLoader
+import pypdf
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaLLM
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA 
