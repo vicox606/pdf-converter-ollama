292697a001f712420e6b80d964b9867c0afd71d9
Vishwa <vishwagreat24@gmail.com>
2026-06-11 13:11:07 +0530

    till prompt

--- main.py
+++ main.py
@@ -7,3 +7,55 @@ from langchain_chroma import Chroma
 from langchain_ollama import OllamaLLM
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA 
+
+# Load the PDF document
+loader = PyPDFLoader(".pdf")
+documents = loader.load()
+
+print(f"Loaded {len(documents)} pages")
+
+# Split the document into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+
+docs = text_splitter.split_documents(documents)
+print(f"Split into {len(docs)} chunks")
+
+# Create embeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-miniLM-L6-v2")
+
+# Store it ChromaDB
+
+vectorestore = Chroma.from_documents(
+    documents=docs,
+    embedding=embedding_model,
+    persist_directory="./chroma_db",
+)
+
+retriever = vectorestore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3},
+)
+
+# Load the Ollama LLM
+
+llm = OllamaLLM(model="llama3.2")
+
+# Custom prompt template
+prompt_template = """
+Your are an Enterprise Knowledge Assistant.
+
+Use Only The provided context to answer the question.
+If the answer is not available in the context, Say:
+
+"I could not find that information in the enterprise document."
+
+Context:{context}
+
+Question:{question}
+
+Answer:
+"""
+
+
+
+
